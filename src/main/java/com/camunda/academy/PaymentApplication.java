b37d3cd79317e3d743b5779f71680acc564bccf4
goenkale <goenkale@gmail.com>
2026-01-21 10:54:04 +0100

    initial

---
+++ src/main/java/com/camunda/academy/PaymentApplication.java
@@ -0,0 +1,67 @@
+package com.camunda.academy;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.camunda.academy.handler.CreditCardServiceHandler;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.worker.JobWorker;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
+
+public class PaymentApplication 
+{
+    private static String CAMUNDA_AUTHORIZATION_SERVER_URL = "https://login.cloud.camunda.io/oauth/token";
+    private static String CAMUNDA_TOKEN_AUDIENCE = "zeebe.camunda.io";
+    private static String CAMUNDA_REST_ADDRESS = "https://[REGION].zeebe.camunda.io/[CLUSTER-ID]";
+    private static String CAMUNDA_GRPC_ADDRESS = "https://[CLUSTER-ID].[REGION].zeebe.camunda.io:443";
+    private static String CAMUNDA_CLIENT_ID = "[CLIENT_ID]";
+    private static String CAMUNDA_CLIENT_SECRET = "[CLIENT_SECRET]";
+
+    public static void main( String[] args )
+    {
+        final OAuthCredentialsProvider credentialsProvider = new OAuthCredentialsProviderBuilder()
+                .authorizationServerUrl(ZEEBE_AUTHORIZATION_SERVER_URL)
+                .audience(ZEEBE_TOKEN_AUDIENCE)
+                .clientId(ZEEBE_CLIENT_ID)
+                .clientSecret(ZEEBE_CLIENT_SECRET)
+                .build();
+        try (final ZeebeClient client = ZeebeClient.newClientBuilder()
+                .grpcAddress(URI.create(ZEEBE_GRPC_ADDRESS))
+                .restAddress(URI.create(ZEEBE_REST_ADDRESS))
+                .credentialsProvider(credentialsProvider)
+                .build()) {
+
+            final Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("reference", "C8_12345");
+            variables.put("amount", Double.valueOf(100.00));
+            variables.put("cardNumber", "1234567812345678");
+            variables.put("cardExpiry", "12/2027");
+            variables.put("cardCVC", "123");
+
+            client.newCreateInstanceCommand()
+                .bpmnProcessId("paymentProcess")
+                .latestVersion()
+                .variables(variables)
+                .send()
+                .join();
+
+            final JobWorker creditCardWorker = client.newWorker()
+                .jobType("chargeCreditCard")
+                .handler(new CreditCardServiceHandler())
+                .timeout(Duration.ofSeconds(10).toMillis())
+                .open();
+            Thread.sleep(10000);
+            
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
